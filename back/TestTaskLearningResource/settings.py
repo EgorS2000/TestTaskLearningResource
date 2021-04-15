7ae155127f4c533e372c2e500440d64ed41b9e6e
EgorS2000 <Egor.Shilkin@icloud.com>
2021-04-15 16:38:47 +0300

    Changed settings.py

--- back/TestTaskLearningResource/settings.py
+++ back/TestTaskLearningResource/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-gd5qvfof_2g#^wu' \
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
